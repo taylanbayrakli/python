ff9fe993efdeec1469ba2f350838b963bfaf13c9
Taylan Bayraklı <taylanbayrakli@192.168.1.70>
2023-09-23 18:21:27 +0300

    test files added

---
+++ meaning = 42.py
@@ -0,0 +1,7 @@
+meaning = 8
+print('')
+
+if meaning > 10:
+    print('right on!')
+else:
+    print('Not today')
---
+++ operators.py
@@ -0,0 +1,18 @@
+# + - * /
+# // round down
+# round()
+# 24 % 5 = 4  , 23 % 7 = 2 
+# 2 ** 3 = 2x2x2 = 8
+#  sayi = 42 , sayi+=1 ---> 43 ## valid for  * - / tambien
+
+# 42 == 42 --> True , 42 == 41 --> False
+# 42 != 42 --> False , 42 != 41 --> True
+# < , > , <= , >=
+
+# x = True 
+# y= False 
+# z =True 
+# not x --> False , not y ---> True 
+# x and y ---> False , x and z ---> True
+#  x or y --> True
+# 
